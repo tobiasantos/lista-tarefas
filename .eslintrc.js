f3da49976aa10a550ae9d5ccf67622f7e6042753
Tobias Santos <tobiassantosn@gmail.com>
2023-03-02 08:23:47 -0300

    merge

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,6 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-<<<<<<< HEAD
-  },
-  extends: ['plugin:react/recommended', 'airbnb'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    'react/jsx-filename-extension': 'off',
-    'react/state-in-constructor': 'off',
-=======
     jest: true,
     node: true,
   },
@@ -46,6 +33,5 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     quotes: 0,
     'react/jsx-key': 'off',
->>>>>>> 0eeb89abf3afc80766c6903c092830828440e4ee
   },
 };
