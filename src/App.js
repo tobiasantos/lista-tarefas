4214da884bcd5551ca90eeaa2244b82cad036210
Tobias Santos <tobiassantosn@gmail.com>
2023-03-02 08:10:34 -0300

    changes updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import Main from './components/Main';
-<<<<<<< HEAD
-=======
-
->>>>>>> 0eeb89abf3afc80766c6903c092830828440e4ee
 import './App.css';
 
 export default function App() {
