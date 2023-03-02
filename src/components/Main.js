23400ba174f960d5414c3ff8972b2272ad425ead
Tobias Santos <tobiassantosn@gmail.com>
2023-03-02 09:09:15 -0300

    edit and delete implementation

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,12 +11,57 @@ import './Main.css';
 export default class Main extends Component {
   state = {
     novaTarefa: '',
-    tarefas: ['Fazer café', 'Beber água', 'Estudar'],
+    tarefas: [],
+    index: -1,
   };
 
-  handleInputChange = (e) => {
+  handleInput = (e) => {
     this.setState({
       novaTarefa: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tarefas, index } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    if (tarefas.indexOf(novaTarefa) !== -1 || novaTarefa === '') return;
+
+    const novasTarefas = [...tarefas];
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa;
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+        novaTarefa: '',
+      });
+    }
+  };
+
+  handleEdit = (e, index) => {
+    const { tarefas } = this.state;
+    this.setState({
+      index,
+      novaTarefa: tarefas[index],
+    });
+  };
+
+  handleDelete = (e, index) => {
+    const { tarefas } = this.state;
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...novasTarefas],
     });
   };
 
@@ -27,25 +72,27 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#" className="form">
-          <input
-            onChange={this.handleInputChange}
-            type="text"
-            value={novaTarefa}
-          />
+        <form onSubmit={this.handleSubmit} action="#" className="form">
+          <input onChange={this.handleInput} type="text" value={novaTarefa} />
           <button type="submit">
             <FaPlus />
           </button>
         </form>
 
         <ul className="tarefas">
-          {tarefas.map((tarefa) => (
+          {tarefas.map((tarefa, index) => (
             <li key={tarefa.id}>
               {tarefa}
-              <div>
-                <FaEdit className="edit" />
-                <FaWindowClose className="delete" />
-              </div>
+              <span>
+                <FaEdit
+                  onClick={(e) => this.handleEdit(e, index)}
+                  className="edit"
+                />
+                <FaWindowClose
+                  onClick={(e) => this.handleDelete(e, index)}
+                  className="delete"
+                />
+              </span>
             </li>
           ))}
         </ul>
